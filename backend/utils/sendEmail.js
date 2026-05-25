1f826394edd580f73b2a6b77ef38058eadafa034
AmarSingh-23 <amarsinghas4688972@gmail.com>
2026-05-25 15:36:49 +0530

    switch to brevo smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,9 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  host: process.env.SMTP_HOST,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_PASSWORD
